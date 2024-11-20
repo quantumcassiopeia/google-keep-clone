1e3cb6a7f166fc7c241d3b02aa942ddf238900e7
Quantum <russewelt@gmail.com>
2024-11-19 21:29:47 -0300

    main

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <header
+      className="
+    bg-amber-400
+    shadow-lg
+    px-5
+    py-5
+    "
+    >
+      <h1
+        className="
+      font-bold
+      text-5xl
+      text-white
+      "
+      >
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
